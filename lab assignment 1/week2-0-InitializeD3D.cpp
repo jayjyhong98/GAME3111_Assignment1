89fa38b19db74308f9abc2159597f4619e808f6c
Jeongyeon Hong <jayjyhong98@gmail.com>
2021-02-05 12:34:34 -0500

    Add Framework
    Add Framework

--- lab assignment 1/week2-0-InitializeD3D.cpp
+++ lab assignment 1/week2-0-InitializeD3D.cpp
@@ -1,8 +1,10 @@
 
 #include "../Common/d3dApp.h"
 #include <DirectXColors.h>
+#include "../Common/GeometryGenerator.h"
 
 using namespace DirectX;
+using Microsoft::WRL::ComPtr;
 
 class InitDirect3DApp : public D3DApp
 {
@@ -10,7 +12,18 @@ public:
 	InitDirect3DApp(HINSTANCE hInstance);
 	~InitDirect3DApp();
 
+private:
+	GeometryGenerator::MeshData		mCone;
+	ComPtr<ID3D12Resource> mConeVtxBuffer;
+	ComPtr<ID3D12Resource> mConeIdxBuffer;
+	D3D12_VERTEX_BUFFER_VIEW mConeVertexBufferView;
+	D3D12_VERTEX_BUFFER_VIEW mConeIndexBufferView;
+
+public:
 	virtual bool Initialize()override;
+
+private:
+	bool CreateCone();
 
 private:
 	virtual void OnResize()override;
@@ -66,6 +79,61 @@ bool InitDirect3DApp::Initialize()
 	if (!D3DApp::Initialize())
 		return false;
 
+	CreateCone();
+
+	return true;
+}
+
+bool InitDirect3DApp::CreateCone()
+{
+	GeometryGenerator	geometryGen;
+
+	mCone = geometryGen.CreateCone(5.f, 10.f, 10, 10);
+
+	const UINT vbBufferSize = (UINT)mCone.Vertices.size() * sizeof(GeometryGenerator::Vertex);
+
+	md3dDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(vbBufferSize),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&mConeVtxBuffer));
+
+	// Copy the triangle data to the vertex buffer.
+	UINT8* pVertexDataBegin;
+	CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+	mConeVtxBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)); //allocates a CPU virtual address range for the resource. 
+	memcpy(pVertexDataBegin, &mCone.Vertices[0], vbBufferSize); //Copies the values of num bytes from the location pointed to by vertices directly to the memory block pointed to by pVertexDataBegin.
+	mConeVtxBuffer->Unmap(0, nullptr);
+
+	//Initialize the vertex buffer view.
+	mConeVertexBufferView.BufferLocation = mConeVtxBuffer->GetGPUVirtualAddress();
+	mConeVertexBufferView.StrideInBytes = sizeof(GeometryGenerator::Vertex);
+	mConeVertexBufferView.SizeInBytes = vbBufferSize;
+
+	const UINT ibBufferSize = (UINT)mCone.Indices32.size() * sizeof(GeometryGenerator::uint32);
+
+	md3dDevice->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(ibBufferSize),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&mConeIdxBuffer));
+
+	// Copy the triangle data to the vertex buffer.
+	UINT8* pIndexDataBegin;
+	CD3DX12_RANGE readRangeIdx(0, 0);        // We do not intend to read from this resource on the CPU.
+	mConeIdxBuffer->Map(0, &readRangeIdx, reinterpret_cast<void**>(&pIndexDataBegin)); //allocates a CPU virtual address range for the resource. 
+	memcpy(pIndexDataBegin, &mCone.Indices32[0], ibBufferSize); //Copies the values of num bytes from the location pointed to by vertices directly to the memory block pointed to by pVertexDataBegin.
+	mConeIdxBuffer->Unmap(0, nullptr);
+
+	//Initialize the vertex buffer view.
+	mConeIndexBufferView.BufferLocation = mConeIdxBuffer->GetGPUVirtualAddress();
+	mConeIndexBufferView.StrideInBytes = sizeof(GeometryGenerator::uint32);
+	mConeIndexBufferView.SizeInBytes = ibBufferSize;
+
 	return true;
 }
 
@@ -105,6 +173,13 @@ void InitDirect3DApp::Draw(const GameTimer& gt)
 
 	// Specify the buffers we are going to render to.
 	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+
+	// Render Shape
+
+
+
+
 
 	// Indicate a state transition on the resource usage.
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
