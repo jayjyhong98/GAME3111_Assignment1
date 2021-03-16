ed4ad6a5c8dae30735d3f552763f0a3fbd704c96
Pauleen Lam <pauleen.lam@outlook.com>
2021-03-15 22:14:22 -0400

    changes to frame and shaders

--- lab assignment 1/FrameResource.h
+++ lab assignment 1/FrameResource.h
@@ -50,7 +50,7 @@ struct FrameResource
 public:
     
     FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount);
-    //FrameResource(const FrameResource& rhs) = delete;
+    FrameResource(const FrameResource& rhs) = delete;
     FrameResource& operator=(const FrameResource& rhs) = delete;
     ~FrameResource();
 
--- lab assignment 1/Shaders/Default.hlsl
+++ lab assignment 1/Shaders/Default.hlsl
@@ -12,7 +12,7 @@
 #endif
 
 #ifndef NUM_SPOT_LIGHTS
-    #define NUM_SPOT_LIGHTS 0
+    #define NUM_SPOT_LIGHTS 8
 #endif
 
 // Include structures and functions for lighting.
